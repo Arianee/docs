ad5f1ad7a7f258a167b46db6e167b6fe0e241ede
alexandre cognard <ac@rlagency.fr>
2019-11-07 18:16:13 +0100

    copyright

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -79,7 +79,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Arianee`,
+  copyright: `Copyright © ${new Date().getFullYear()} .Arianee Project`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
